946620981af3d035d0c96ec3be903409f725b2bc
Calum Moir <CalumbiaM@Calums-MacBook-Pro-2.local>
2018-10-12 21:36:05 +0100

    Fix create of postgres table. Fix parseint spelling error

--- server/index.js
+++ server/index.js
@@ -20,8 +20,9 @@ const pgClient = new Pool({
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.log(err));
+pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err));
 
 // Redis Client Setup
 const redis = require('redis');
@@ -38,22 +39,22 @@ app.get('/', (req, res) => {
     res.send('Hi');
 });
 
-app.get('/values/all', async (req, res) => {
+app.get('/values/all', async(req, res) => {
     const values = await pgClient.query('SELECT * FROM values');
 
     res.send(values.rows);
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/values/current', async(req, res) => {
     redisClient.hgetall('values', (err, values) => {
         res.send(values);
     });
 });
 
-app.post('/values', async (req,res) => {
+app.post('/values', async(req, res) => {
     const index = req.body.index;
 
-    if (parstInt(index) > 40) {
+    if (parseInt(index) > 40) {
         return res.status(422).send('Index too high');
     }
 
